a11565a5ecebf4b8866436df19ccb42e90bb2b13
Spydr06 <spydr06@web.de>
2023-11-05 17:55:43 +0100

    full reset

--- src/libastatine/ast/object.c
+++
@@ -1,8 +0,0 @@
-#include "object.h"
-
-Object_T* init_object(Module_T* module, Token_T* token)
-{
-    Object_T* obj = arena_calloc(&module->memory, 1, sizeof(Object_T));
-    obj->token = token;
-    return obj;
-}
--- src/libastatine/passes.h
+++
@@ -1,8 +0,0 @@
-#ifndef ASTATINE_PASSES_H
-#define ASTATINE_PASSES_H
-
-#include <astatine.h>
-
-typedef AstatinePassFn_T PassFn_T;
-
-#endif /* ASTATINE_PASSES_H */
